cce56dcf7f842ba0eda4fcf09476e397076b782f
Logon Aluno <logonrmlocal@fiap.com.br>
2019-05-15 23:02:43 -0300

    aula fiap

---
+++ src/test/java/br/com/fiap/carservice/CarResourceMockTest.java
@@ -0,0 +1,43 @@
+package br.com.fiap.carservice;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class CarResourceMockTest {
+    @Mock
+    private CarResource carResource;
+
+    @Test
+    public void shouldFindCarByColor(){
+        String color = "preta";
+        String model = "hb20";
+
+        Car car = new Car();
+        car.setColor(color);
+        car.setModel(model);
+
+        List<Car> cars = Arrays.asList(car);
+
+        //when
+        Mockito.when(carResource.findCarByColor(color)).thenReturn(cars);
+
+        //executar
+        List<Car> carsResponse = carResource.findCarByColor(color);
+
+        assertEquals(carsResponse.get(0).getColor(), color);
+        assertEquals(carsResponse.get(0).getModel(), model);
+
+
+    }
+}
